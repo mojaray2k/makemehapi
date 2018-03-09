f8de36f353ec6d7d98cf3f0960b26d5c9f224c9d
Amen Ra <ara@envistacorp.com>
2018-03-09 01:32:22 -0500

    feat: Adding exercise 2 of 13
    Create a hapi server that listens on a port passed from the command line and outputs "Hello [name]"
    where [name] is replaced with the path parameter supplied to GET /{name}

--- program.js
+++ program.js
@@ -6,7 +6,7 @@ const server = new Hapi.Server({
 });
 
 server.route({
-    path: '/', 
+    path: '/{name?}', 
     method:'GET', 
     handler: handler
 });
@@ -15,8 +15,8 @@ function handler(request, h) {
     
     // Request has all information
     // a string can be returned
-
-    return 'Hello World!';
+    
+    return `Hello ${request.params.name} `;
 }
 
 async function start(){
@@ -46,18 +46,20 @@ start();
             });
 
             server.route({
-                path: '/',
+                path: '/{name}',
                 method: 'GET',
-                handler: (request, h) => {
-                    return 'Hello World!';
+                handler: function (request, h) {
+                    return `Hello ${request.params.name} `;
+
+                    // a more secure alternative is this:
+                    //
+                    // return `Hello ${encodeURIComponent(request.params.name)}`;
+                    //
+                    // encodeURIComponent escapes all characters except the following: alphabetic, decimal digits, - _ . ! ~ * ' ( )
+                    // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+                    // for more details why you should call encodeURIComponent on any user-entered parameter
                 }
-            });
+        });
 
-            await server.start();
-
-            console.log(`Server running at: ${server.info.uri}`);
-        } catch (error) {
-            console.log(error);
-        }
-    })();
+    await server.start();
  */
