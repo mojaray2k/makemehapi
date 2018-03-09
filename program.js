d8c53e24913c556e193d9767d1e05471ebb555d3
Amen Ra <ara@envistacorp.com>
2018-03-09 01:11:21 -0500

    feat: First Hello World Exercise done
    This is the first POC of using the current addition of Hapi

--- program.js
+++ program.js
@@ -1,6 +1,6 @@
-var Hapi = require('hapi');
+const Hapi = require('hapi');
 
-var server = new Hapi.Server({
+const server = new Hapi.Server({
     host: 'localhost',
     port: Number(process.argv[2] || 8080)
 });
@@ -16,9 +16,48 @@ function handler(request, h) {
     // Request has all information
     // a string can be returned
 
-    return 'a string in the response';
+    return 'Hello World!';
 }
 
-await server.start();
+async function start(){
+
+    try {
+        await server.start();
+    }
+    catch(err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+}
+
+start();
     
-console.log('Server running at:', server.info.uri);
+/**
+ * Here the comparison with makemehapi
+    const Hapi = require('hapi');
+
+    (async () => {
+        try {
+            const server = Hapi.Server({
+                host: 'localhost',
+                port: Number(process.argv[2] || 8080)
+            });
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: (request, h) => {
+                    return 'Hello World!';
+                }
+            });
+
+            await server.start();
+
+            console.log(`Server running at: ${server.info.uri}`);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+ */
